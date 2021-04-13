23d3354f2465909c45aeb28b603ec42c7f449f5a
Pratik1005 <pratik.devle98@gmail.com>
2021-04-13 13:50:53 +0530

    Initial commit
    Created cli quiz about myself.

---
+++ index.js
@@ -0,0 +1,34 @@
+var readlineSync = require('readline-sync');
+var score = 0;
+function play(question, answer){
+  var userAns = readlineSync.question(question);
+  if(userAns.toUpperCase() === answer.toUpperCase()){
+    console.log("You are right!");
+    score++;
+  }else{
+    console.log("You are wrong!");
+  }
+  console.log("Your current score: " + score);
+  console.log("------------");
+}
+
+var questions =  [
+{
+  question: "What is my name? ",
+  answer: "Pratik"
+},
+{
+  question: "Where do I live? ",
+  answer: "Mumbai"
+},
+{
+  question: "Is my age greater than 25? ",
+  answer: "No"
+}
+];
+
+for(var i=0; i<questions.length; i++){
+  play(questions[i].question, questions[i].answer);
+}
+
+console.log("YOUR FINAL SCORE: "+score);
